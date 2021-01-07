12f1e0f629df5269cae366919623f884bfc79dee
李萌 <lim16@mingyuanyun.com>
2021-01-07 10:14:21 +0800

    feat: 布局

---
+++ src/layout/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <el-container>
+    <el-aside width="200px"><app-aside /></el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main><router-view /></el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'AdvertIndex',
+  components: {
+    AppAside: () => import('./components/app-aside.vue')
+  },
+  data() {
+    return {}
+  }
+})
+</script>
+<style lang='scss' scoped>
+.el-container {
+  min-height: 100vh;
+  min-width: 980px;
+}
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+</style>
